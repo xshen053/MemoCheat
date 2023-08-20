a908355849ca221540ba418f8d4c68a43e44f1f6
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-08-19 22:24:42 -0700

    fixed typo MemoryListCreateView

--- src/ebbinghaus_planner/memory/urls.py
+++ src/ebbinghaus_planner/memory/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import MemoryListCreateView
 
 urlpatterns = [
-    path('memory/', TaskListCreateView.as_view(), name='memory-list-create'),
+    path('memory/', MemoryListCreateView.as_view(), name='memory-list-create'),
 ]
