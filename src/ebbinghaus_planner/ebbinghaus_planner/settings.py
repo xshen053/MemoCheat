3ef6219ad93025f3e4825dc55ef746aa01f24ae2
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-08-19 22:14:57 -0700

    removed memory

--- src/ebbinghaus_planner/ebbinghaus_planner/settings.py
+++ src/ebbinghaus_planner/ebbinghaus_planner/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "ebbinghaus_planner.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ebbinghaus_db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
