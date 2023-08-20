b504804c4ae6f8364f484b90025119d45f7bc999
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-08-19 22:22:17 -0700

    added memory view

--- src/ebbinghaus_planner/memory/views.py
+++ src/ebbinghaus_planner/memory/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import memory
+from .serializers import MemorySerializer
 
-# Create your views here.
+class MemoryListCreateView(generics.ListCreateAPIView):
+    queryset = memory.objects.all()
+    serializer_class = MemorySerializer
