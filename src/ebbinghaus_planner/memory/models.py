87e9705daaa201fb63b5dd138acd78651993f828
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-08-19 22:21:55 -0700

    added memory database model

--- src/ebbinghaus_planner/memory/models.py
+++ src/ebbinghaus_planner/memory/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class memory(models.Model):
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)
+    review_date = models.DateTimeField()  # You'll calculate this based on the Ebbinghaus curve
