f4de83333875f59bbec7df02f2ded8b9a1e52fa0
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-10-17 14:29:07 -0700

    FEATURE: add the day that card added to review date

--- src/ebbinghaus_planner/memory/utils.py
+++ src/ebbinghaus_planner/memory/utils.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
 
 def calculate_review_dates(created_date):
-    plan = [1, 2, 4, 7, 14, 21, 30, 45, 60, 90, 120, 150]  # example intervals
+    plan = [0, 1, 2, 4, 7, 14, 21, 30, 45, 60, 90, 120, 150]  # example intervals
     return [created_date + timedelta(days=interval) for interval in plan]
