174932dc0b9be5c38ae4b61ee0a4c6a1a75f8464
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-11-11 13:45:17 -0800

    update latest backup.sh

--- backup.sh
+++ backup.sh
@@ -1,2 +1,6 @@
 # docker exec -e PGPASSWORD=your_password -t ebbinghaus-db pg_dump -U your_username -d your_database > ebbinghaus_backup.sql
 docker exec -e PGPASSWORD=your_password -t ebbinghaus-db pg_dump -U postgres -d postgres > ebbinghaus_backup.sql
+
+git add ebbinghaus_backup.sql
+git commit -m "updated sql"
+git push
