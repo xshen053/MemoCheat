098f56403a5fa63cc84aa376b7b8cb470105661a
Xiaxi Shen <shenxiaxi26@gmail.com>
2023-12-03 17:16:50 -0800

    add router

--- src/ebbinghaus_client/src/App.js
+++ src/ebbinghaus_client/src/App.js
@@ -1,29 +1,42 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import MemoryCalendar from "./components/MemoryCalendar";
 import AddMemory from "./components/AddMemory";
 import TodayReview from "./components/TodayReview";
 import TomorrowReview from "./components/TomorrowReview";
-import CodingProblemCount from "./components/IsCodingProblem"
+import CodingProblemCount from "./components/IsCodingProblem";
+import MemoryTablePage from "./components/MemoryTablePage"; // Import your new component
+
 function App() {
   return (
-    <div className="App">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center", // Align the items to the center
-          gap: "20px",
-          maxWidth: "800px", // Set a maximum width for the container
-          margin: "0 auto", // Center the container on the screen
-        }}
-      >
-        <CodingProblemCount />
-        <AddMemory />
-        <TodayReview />
-        <TomorrowReview />
+    <Router>
+      <div className="App">
+        <nav style={{ marginBottom: '20px' }}>
+          <Link to="/">Home</Link> | <Link to="/memory-table">Memory Table</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                gap: "20px",
+                maxWidth: "800px",
+                margin: "0 auto",
+              }}
+            >
+              <CodingProblemCount />
+              <AddMemory />
+              <TodayReview />
+              <TomorrowReview />
+            </div>
+          } />
+          <Route path="/memory-table" element={<MemoryTablePage />} />
+        </Routes>
+        <MemoryCalendar style={{ marginTop: "1000px" }} />
       </div>
-      <MemoryCalendar style={{ marginTop: "1000px" }} />
-    </div>
+    </Router>
   );
 }
 
